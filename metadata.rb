398ca76420907f7ca9304c493f54a0f6c5204d4a
psamlong <psamlong@hinnoya.fr>
2014-07-16 18:01:44 +0200

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'test-cookbook'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures test-cookbook'
+long_description 'Installs/Configures test-cookbook'
+version          '0.1.0'
+
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: test-cookbook
+# Recipe:: default
+#
+# Copyright (C) 2014 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
