6e557e750c7ed64edc787015e4efa3b8fa5bd57e
psamlong <psamlong@hinnoya.fr>
2014-07-17 11:06:27 +0200

    Use chef cache directory for tarball

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,7 +10,7 @@
 include_recipe "apache2"
 include_recipe "mysql::server"
 
-remote_file "/tmp/mediawiki-1.23.1.tar.gz" do
+remote_file "#{Chef::Config[:file_cache_path]}/mediawiki-1.23.1.tar.gz" do
   source 'https://releases.wikimedia.org/mediawiki/1.23/mediawiki-1.23.1.tar.gz'
 end
 
@@ -18,7 +18,7 @@ bash "install_mediawkiki" do
   user "root"
 	cwd node['apache']['docroot_dir']
 	code <<-EOH
-	  tar -zxf /tmp/mediawiki-1.23.1.tar.gz
+	  tar -zxf #{Chef::Config[:file_cache_path]}/mediawiki-1.23.1.tar.gz
 	EOH
 	action :nothing
 end
