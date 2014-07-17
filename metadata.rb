0b7fb1d0ead194fe2f01c98decb3a1de3ee47137
psamlong <psamlong@hinnoya.fr>
2014-07-17 10:25:26 +0200

    Enable apache2 defaut recipe in test recipe

--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,6 @@ license          'All rights reserved'
 description      'Installs/Configures test-cookbook'
 long_description 'Installs/Configures test-cookbook'
 version          '0.1.0'
+
 depends          'apache2'
 depends          'mysql'
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,6 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+include_recipe "apache2"
+#include_recipe "mysql"
